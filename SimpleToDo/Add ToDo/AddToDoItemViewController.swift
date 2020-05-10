090d54adf3f57fca019ababd5be5b52793e005ff
Andrew Miotke <andrewmiotke@gmail.com>
2020-05-10 13:03:08 -0700

    Add Task: Started creating the UI for the add task view.
    * Created the stackview for the task complete toggle

--- SimpleToDo/Add ToDo/AddToDoItemViewController.swift
+++ SimpleToDo/Add ToDo/AddToDoItemViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class AddToDoItemViewController: UIViewController {
+    
+    let taskTitleTextField = UITextField()
+    let taskDetailsTextView = UITextView()
+    let taskCompleteSwitch = UISwitch()
+    let taskCompleteLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationController()
+        addSubviews()
+        configureUI()
+        
+        taskCompleteLabel.text = "Task complete: "
     }
     
     private func setupNavigationController() {
         let dismissButtonImage = UIImage(systemName: "x.circle.fill")
-        navigationItem.title = "Add item"
+        navigationItem.title = "Add task"
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: dismissButtonImage, style: .done, target: self, action: #selector(dismissView))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveItem))
     }
@@ -28,5 +37,31 @@ class AddToDoItemViewController: UIViewController {
     
     @objc func dismissView() {
         self.dismiss(animated: true)
+    }
+    
+    func addSubviews() {
+        view.addSubview(taskTitleTextField)
+        view.addSubview(taskDetailsTextView)
+        view.addSubview(taskCompleteSwitch)
+        view.addSubview(taskCompleteLabel)
+        
+        taskTitleTextField.translatesAutoresizingMaskIntoConstraints = false
+        taskDetailsTextView.translatesAutoresizingMaskIntoConstraints = false
+        taskCompleteSwitch.translatesAutoresizingMaskIntoConstraints = false
+        taskCompleteLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func configureUI() {
+        let taskCompleteStackView = UIStackView(arrangedSubviews: [taskCompleteLabel, taskCompleteSwitch])
+        taskCompleteStackView.axis = .horizontal
+        
+        view.addSubview(taskCompleteStackView)
+        taskCompleteStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            taskCompleteStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            taskCompleteStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
     }
 }
