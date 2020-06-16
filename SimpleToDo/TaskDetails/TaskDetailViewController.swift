d824e1ad7d695dc0db2e7cb7392bcab9db8803a6
Andrew Miotke <andrewmiotke@gmail.com>
2020-06-15 18:33:05 -0700

    Task Detail: Adding a view controller for the detail view of a task

---
+++ SimpleToDo/TaskDetails/TaskDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskDetailViewController.swift
+//  SimpleToDo
+//
+//  Created by Andrew Miotke on 6/15/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
