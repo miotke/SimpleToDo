4e88ae3eb1faa3133d0533b2aa158f3dd1955b81
Andrew Miotke <andrewmiotke@gmail.com>
2020-07-07 18:07:07 -0700

    Task List Cell: Started the rewrite on the task list cell which is the main TableView cell

--- SimpleToDo/TaskList/Cell/STTableViewCell.swift
+++ SimpleToDo/TaskList/Cell/STTableViewCell.swift
@@ -12,14 +12,39 @@ class STTableViewCell: UITableViewCell {
     
     static let reuseId = "STTableViewCell"
     
+    let taskTitle = UILabel()
+    let dateLabel = UILabel()
+    let taskStatusIndicatorImageView = UIImageView()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: "STTableViewCell")
+        
+        layoutUI()
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func layoutUI() {
+        let padding: CGFloat = 8
+        let stackView = UIStackView(arrangedSubviews: [taskTitle, dateLabel])
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        
+        
+        addSubview(stackView)
+        addSubview(taskStatusIndicatorImageView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        taskStatusIndicatorImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // MARK: STOPPING POINT: Finishing writing the constraints
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: padding),
+        ])
     }
     
     required init?(coder: NSCoder) {
