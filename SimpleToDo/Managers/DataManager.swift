74d258d7129b2dfa1aae94d9959ed611a6b90163
Andrew Miotke <andrewmiotke@gmail.com>
2020-07-15 12:57:42 -0700

    Creating a data manger to be the middle man for passing data

---
+++ SimpleToDo/Managers/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  SimpleToDo
+//
+//  Created by Andrew Miotke on 7/15/20.
+//  Copyright © 2020 andrewmiotke. All rights reserved.
+//
+
+import Foundation
+
+class DataManger {
+    
+}
